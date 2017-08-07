815d12eb9e3fc8f636b354d6b44f9d9d63417b26
Henri Knafo <cmoi40@live.ca>
2017-08-07 06:00:01 -0400

    revealed5

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World it works!');
 });
-app.listen(3001, function () {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
